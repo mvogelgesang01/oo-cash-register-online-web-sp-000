e9064a3439b95d9214809ffe9fd92a13f9e127fa
Melissa Vogelgesang <mvogelgesang01@gmail.com>
2021-06-05 17:07:44 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -9,4 +9,14 @@ class CashRegister
     @items = []
     @transactions = []
  end
-end
+
+def add_item(title, price, quantity = 1)
+    @total += price * quantity
+    @transactions << price
+    i = quantity
+    until i == 0 do
+      @items << title
+      i -= 1
+    end
+  end
+  
