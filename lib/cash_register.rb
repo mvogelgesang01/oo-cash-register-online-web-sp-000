a02f6cd864381f122e9db720f9f11ad8096339c1
Melissa Vogelgesang <mvogelgesang01@gmail.com>
2021-06-05 17:02:21 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,11 @@
+class CashRegister
+  attr_accessor :total, :discount, :quantity, :price
 
+  def initialize (discount = 0)
+    @total = 0
+    @discount = discount
+    @price = price
+    @quantity = quantity
+    @items = []
+    @transactions = []
+  end
