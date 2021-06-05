00ce59be57e9c790eb400967a55d58092b54bb19
Melissa Vogelgesang <mvogelgesang01@gmail.com>
2021-06-05 17:17:35 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,39 +1,28 @@
 class CashRegister
-  attr_accessor :total, :discount, :quantity, :price
 
-  def initialize(discount = 0)
-    @total = 0
-    @discount = discount
-    @price = price
-    @quantity = quantity
-    @items = []
-    @transactions = []
+  attr_accessor :total, :discount, :items, :last_transaction
+
+  def initialize(discount = nil)
+    self.total = 0
+    self.discount = discount
+    self.items = []
   end
 
   def add_item(title, price, quantity = 1)
-    @total += price * quantity
-    @transactions << price
-    i = quantity
-    until i == 0 do
-      @items << title
-      i -= 1
-    end
+    self.total += price * quantity
+    quantity.times { self.items << title }
+    self.last_transaction = [title, price, quantity]
   end
 
   def apply_discount
-    if @discount == 0
-      "There is no discount to apply."
-    else
-      self.total -= (0.01 * @discount * @total).to_i
-      "After the discount, the total comes to $#{self.total}."
-    end
-  end
-
-  def items
-    @items
+    self.total == 0 ? "There is no discount to apply." : "After the discount, the total comes to $#{self.total = self.total * 8 / 10 }."
   end
 
   def void_last_transaction
-    self.total = @total - @transactions.pop
+    self.total -= self.last_transaction[1] * self.last_transaction[2]
+    self.last_transaction[2].times do
+      self.items.delete_at(self.items.index(self.last_transaction[0]) || self.items.count)
+    end
   end
+
 end
